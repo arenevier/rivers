b2c4125913308ff29ba2010cb42aa9ab3acdb903
arno <arno@renevier.net>
2010-12-29 13:53:44 +0100

    remove france.osm.bz2 once it is uncompressed

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 wget http://download.geofabrik.de/osm/europe/france.osm.bz2
 bunzip2 france.osm.bz2
+rm france.osm.bz2
 python river_import.py france.osm
 python river_output.py
 ssh renevier.net "rm -rf /var/httpd/main/maps/rivers"
